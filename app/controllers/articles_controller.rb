c72bbb699f2484b2510c7c7de1b3b9a57bb4ee47
An Le <an.lnthien@gmail.com>
2018-09-29 10:47:23 +0700

    add articles create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,8 @@
 class ArticlesController < ApplicationController
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :text))
+    @article.save
+
+    render json: @article, status: :ok
+  end
 end
